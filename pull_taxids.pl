b23d878ba418a4de7489103841a2410aea577c08
atcg <evanmelstad@ucla.edu>
2012-11-01 00:02:31 -0700

    Finished pull_taxids.pl. Started working on getting from higher level taxIDs to lists of GIs for lower-level child nodes

--- pull_taxids.pl
+++ pull_taxids.pl
@@ -1,27 +1,54 @@
 #!/usr/bin/perl
 
+#This subroutine recursively searches through all child nodes of a taxon
+#You feed the subroutine a single NCBI taxon ID (say a genus), and it will return
+#an array of taxon IDs corresponding to all of the species within that genus.
+#If you need to get other information (such as species names or species/subspecies
+#ranks), then you can easily alter the subroutine to return an array of
+#Bio::DB::Taxonomy objects instead, which may be accessed downstream.
+
+#MUST USE DOWNLOADED TAXONOMY FLATFILE (update this regularly). The use of entrez
+#as the source of the factory is apparently broken in BioPerl.
+
+#usage example (for the genus anolis): my @childSpecies = getChildSpecies(28376);
+
 use strict;
 use warnings;
 use Bio::DB::Taxonomy;
 
-#MUST USE DOWNLOADED TAXONOMY FLATFILE
-my $dbdir = '/home/evan/Desktop/src/MitoNuc/db'; #downloaded data from NCBI taxdump into this directory
-my $db = Bio::DB::Taxonomy->new(-source => 'flatfile',
-                                 -nodesfile => "$dbdir/nodes.dmp",
-                                 -namesfile => "$dbdir/names.dmp",
-                                 );
-my $taxa = $db->get_taxon(-taxonid => 8948);
-my @childNodes = $db->get_all_Descendents($taxa);
 
-#Extract the elements of @childNodes that are of species rank into new array
-my @childSpecies;
-foreach my $subnode (@childSpecies) {
-    #if $subnode is a species:
-        #push to @childSpecies
+my @childSpecies = getChildSpecies(50);
+print scalar(@childSpecies) . "\n";
+
+sub getChildSpecies
+{
+
+#first check to make sure our input parameters are correct:
+    if (scalar(@_) == 1 && $_[0] =~ /^\d+/)
+    {    
+        my $dbdir = 'db'; #this is a dir containing nodes.dmp and names.dmp from ncbi
+        my $db = Bio::DB::Taxonomy->new(-source => 'flatfile',
+                                        -nodesfile => "$dbdir/nodes.dmp",
+                                        -namesfile => "$dbdir/names.dmp",
+                                        );
+        my $taxa = $db->get_taxon(-taxonid => $_[0]);
+        my @childNodes = $db->get_all_Descendents($taxa);
+        
+        #Extract the elements of @childNodes that are of species rank into new array
+        my @childSpecies;
+        foreach my $subnode (@childNodes)
+        {
+            if ($subnode->rank eq 'species' or $subnode->rank eq 'subspecies') {
+                #add taxon to @childSpecies
+                push(@childSpecies, $subnode->id);
+            }
+        }
+        return (@childSpecies);
+    #    print join("\n", map { $_->id . " " . $_->rank . " " . 
+    #    $_->scientific_name } @childSpecies), "\n";
+    #    print "total species/subspecies: ", scalar(@childSpecies), "\n";
+    } else {
+        die "Must provide a single integer value (taxon ID) to getChildSpecies";
+    }
+    
 }
-
-
-print join("\n", map { $_->id . " " . $_->rank . " " .
-$_->scientific_name } @childNodes), "\n";
-
-print "Total number of subnodes under parent taxonomy node: ", scalar(@childNodes), "\n";
