7f2e53389c978e1e09d94b11364d6c9b30600849
atcg <evanmelstad@ucla.edu>
2013-02-05 15:57:40 -0800

    fixed a file name

--- lib/remove_mito_gis_from_gi_lists.pl
+++ lib/remove_mito_gis_from_gi_lists.pl
@@ -8,7 +8,7 @@ my $taxID;
 GetOptions ("taxid=i" => \$taxID);
 
 #create a big hash for all the mitochondrial GIs
-print "Creating a hash for all mitochondrial GIs from data/mitoGIs.txt.\n";
+print "Creating a hash for all mitochondrial GIs from data/mitoGIs_$taxID.txt.\n";
 my %allMitoHash;
 open (my $allMitoGiFile, "<", "data/mitoGIs_$taxID.txt") || die "Couldn't open the full mito GI file for reading: $!";
 while (my $mitoGI = <$allMitoGiFile>) {
